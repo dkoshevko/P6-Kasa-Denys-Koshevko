261b0b9ea7a4717c2b9cffb021e93aa0cddd80d0
dkoshevko <121438170+dkoshevko@users.noreply.github.com>
2023-05-23 12:21:57 +0200

    page 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,14 @@
+import '../styles/NotFound.css';
+import { Link } from 'react-router-dom';
+
 export default function NotFound() {
     return (
-        <div>
-            <h1>404</h1>
-        </div>
+        <main>
+            <h1 className='fof'>404</h1>
+            <h2 className='fof-message'>Oups! La page que vous demandez n'existe pas.</h2>
+            <Link to={`/`} className='back-home'>
+            Retourner sur la page d’accueil
+            </Link>        
+        </main>
     )
 }
